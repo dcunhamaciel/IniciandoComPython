e08df95d47829b459c4220c04fc6f65bd2c28ec8
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-18 19:32:21 -0300

    String

--- string.py
+++ string.py
@@ -3,14 +3,14 @@ print('hello')
 print("Levis's")
 print("Hello\nWesley")
 
-print("C:\number.txt")
-print(r"C:\number.txt")
+print("C:\number.txt")  # o \n quebra a linha
+print(r"C:\number.txt") # o "r" antes do \n evita a quebra de linha (r de raw)
 
 name = "Diego"
 last_name = "Maciel"
 
 print(name + " " + last_name)
-print((name + " " + last_name) * 3)
+print((name + " " + last_name) * 3) # triplica a string
 
 print(name[2])
 print(name[0:3])
