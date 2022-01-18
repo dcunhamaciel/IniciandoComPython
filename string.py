ebcc60ea7a508eeacf5a48ecc0fa5c3b2d167f14
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-17 21:07:15 -0300

    String

---
+++ string.py
@@ -0,0 +1,19 @@
+print("hello")
+print('hello')
+print("Levis's")
+print("Hello\nWesley")
+
+print("C:\number.txt")
+print(r"C:\number.txt")
+
+name = "Diego"
+last_name = "Maciel"
+
+print(name + " " + last_name)
+print((name + " " + last_name) * 3)
+
+print(name[2])
+print(name[0:3])
+print(name[:3])
+print(name[3:5])
+print(name[3:])
